a6f26b3e666c5a48c33f012756e1e4133e213638
BOTZ4YOU <fahrigaannzz@gmail.com>
2021-12-22 14:50:28 +0700

    FahriBotzWaV7

---
+++ shop/allpayment.js
@@ -0,0 +1,38 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY FAHRI BOTZ
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0857 7267 4695
+2. BCA 
+	ㅁ 3901 0857 7267 4695
+3. BNI 
+	ㅁ 881 0857 7267 4695
+4. PERMATA
+	ㅁ 852 0857 7267 4695
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 7267 4695
+2. OVO
+	ㅁ 0857 7267 4695
+3. DANA
+	ㅁ 0857 7267 4695
+	
+Semuanya atas nama F****S****
+
+*📲 PULSA*
+
+1. IM3/INDOSAT
+	ㅁ 0857 7267 4695 (+5K)
+2. SMARTFREN
+	ㅁ 0889 0570 3349 (+5K)
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,36 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0857 7267 4695
+2. BCA 
+	ㅁ 3901 0857 7267 4695
+3. BNI 
+	ㅁ 881 0857 7267 4695
+4. PERMATA
+	ㅁ 852 0857 7267 4695
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0857 7267 4695
+2. OVO
+	ㅁ 0857 7267 4695
+3. DANA
+	ㅁ 0857 7267 4695
+	
+Semuanya atas nama F****S****
+
+*📲 PULSA*
+
+1. IM3/INDOSAT
+	ㅁ 0857 7267 4695 (+5K)
+2. SMARTFREN
+	ㅁ 0889 0570 3349 (+5K)
+`
+	}
+
+exports.donasibot = donasibot
+ 
